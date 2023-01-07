861b4eaab1b6d2b7c6d7149a49f78b5e517566da
bokob <bmc0520@naver.com>
2023-01-07 17:20:01 +0900

    다시 시작

--- Homework/React-homework-1/src/App.js
+++ Homework/React-homework-1/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      시작
+      <div>안녕</div>
+      <Routes>
+        <Route path="/" element={<div>ㅋ</div>} />
+        <Route path="/1" element={<div>1</div>} />
+      </Routes>
     </div>
   );
 }
