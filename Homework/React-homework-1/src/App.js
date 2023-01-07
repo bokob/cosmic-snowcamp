997896e252d3d381571369d1f6bf1b51a7f49d05
bokob <bmc0520@naver.com>
2023-01-07 19:29:45 +0900

    Route and Link prepare

--- Homework/React-homework-1/src/App.js
+++ Homework/React-homework-1/src/App.js
@@ -8,24 +8,78 @@ import './css/B.Font.min.css';
 import './css/B.Home.min.css';
 import './css/B.Message.min.css';
 import './css/B.min.css';
+import Introduce from './pages/Introduce';
+import { useState } from 'react';
 
 function App() {
+  let [tab, setTab] = useState(0);
+
   return (
     <div className="App">
-      <div>
-        {/*공통된 부분*/}
-        <ServiceHeaderHome />
-        <WrapSideMenu />
-        <WrapHome />
-        <Layer />
-      </div>
-
+      <div>{/*공통된 부분*/}</div>
+      {/* <Introduce /> */}
       <Routes>
-        <Route path="/" element={<div>ㅋ</div>} />
-        <Route path="1" element={<div>1</div>} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <Common />
+            </div>
+          }
+        >
+          <Route path="info" element={<div>소개</div>} />
+          <Route path="articles" element={<div>글</div>} />
+          <Route path="works" element={<div>사진</div>} />
+        </Route>
         <Route path="*" element={<div>404임 꺼지셈</div>} />
       </Routes>
     </div>
+  );
+}
+
+function Common() {
+  return (
+    <>
+      <ServiceHeaderHome />
+      <WrapSideMenu />
+      <WrapHome />
+      <Layer />
+      <TabContent />
+      <Outlet></Outlet>
+    </>
+  );
+}
+
+function Debug() {
+  console.log('확인');
+  return <div>확인</div>;
+}
+
+function TabContent(props) {
+  return (
+    <>
+      <div className="tab_contents">
+        <strong className="screen_out">작가프로필 하위메뉴</strong>
+        <ul id="contentsTab" className="list_tab">
+          <li>
+            <Link to="info" className="infoTab link_tab #info_open">
+              <span className="txt_tab">작가소개</span>
+            </Link>
+          </li>
+          <li className="articles">
+            <Link to="articles" className="articleTab link_tab">
+              <span className="txt_tab">글 (개수)</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="works" className="magazineTab link_tab">
+              <span className="txt_tab">작품 (개수)</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      {/* 구분선 밑부분 부터 main 태그   article 들어가는 곳임*/}
+    </>
   );
 }
 
--- Homework/React-homework-1/src/components/WrapHome.js
+++ Homework/React-homework-1/src/components/WrapHome.js
@@ -5,14 +5,14 @@ function WrapHome() {
       <input id="profileUserId" type="hidden" value="5tdm" />
       <input id="profileUserName" type="hidden" value="박재성" />
       <input id="blockedUser" type="hidden" value="false" />
-      <header class="header_head_menubar #my_profile">
-        <h2 class="screen_out">박재성</h2>
-        <div class="cover_image"></div>
+      <header className="header_head_menubar #my_profile">
+        <h2 className="screen_out">박재성</h2>
+        <div className="cover_image"></div>
 
-        <div class="wrap_profile">
-          <div class="bloger_thumb">
+        <div className="wrap_profile">
+          <div className="bloger_thumb">
             <img
-              class="profileUserImage img_thumb"
+              className="profileUserImage img_thumb"
               src="https://avatars.githubusercontent.com/u/85085804?v=4"
               width="100"
               height="100"
@@ -20,53 +20,56 @@ function WrapHome() {
               data-image="https://avatars.githubusercontent.com/u/85085804?v=4"
             />
             <input
-              class="profileUserImageUrl"
+              className="profileUserImageUrl"
               type="hidden"
               value="http://t1.daumcdn.net/brunch/service/guest/image/V4wdZX1aq-c_w0D7_taGTXkIgrk"
             />
           </div>
-          <div class="wrap_profile_desc">
-            <strong class="profileUserName tit_bloger">복무창</strong>
-            <span class="blog_cpeg">
-              <em class="screen_out">소속</em>
-              <span class="txt_info">충북대학교</span> <em class="screen_out">직업</em>
-              <span class="txt_info">3학년</span>{' '}
+          <div className="wrap_profile_desc">
+            <strong className="profileUserName tit_bloger">복무창</strong>
+            <span className="blog_cpeg">
+              <em className="screen_out">소속</em>
+              <span className="txt_info">충북대학교</span> <em className="screen_out">직업</em>
+              <span className="txt_info">3학년</span>{' '}
             </span>
-            <dl class="blog_count ">
-              <dt class="screen_out">브런치 정보</dt>
+            <dl className="blog_count ">
+              <dt className="screen_out">브런치 정보</dt>
               <dd>
-                <a href="/@javajigi/follower" class="link_count #follower">
-                  <em class="txt_g">구독자</em>
-                  <span class="num_count">1,270</span>
+                <a href="/@javajigi/follower" className="link_count #follower">
+                  <em className="txt_g">구독자</em>
+                  <span className="num_count">0</span>
                 </a>
               </dd>
               <dd>
-                <a href="/@javajigi/following" class="link_count #following">
-                  <em class="txt_g">관심작가</em>
-                  <span class="num_count">26</span>
+                <a href="/@javajigi/following" className="link_count #following">
+                  <em className="txt_g">관심작가</em>
+                  <span className="num_count">0</span>
                 </a>
               </dd>
             </dl>
           </div>
-          <div class="wrap_profile_btn">
-            <span class="#my_follow">
-              <button type="button" class="btn_type btn_default btn_profile btnFollow #p_follow">
+          <div className="wrap_profile_btn">
+            <span className="#my_follow">
+              <button
+                type="button"
+                className="btn_type btn_default btn_profile btnFollow #p_follow"
+              >
                 구독하기
               </button>
             </span>
 
-            <div class="more_control">
+            <div className="more_control">
               {' '}
               {/*<!-- 메뉴더보기 클릭시 ctrl_on 클래스 추가 -->*/}
-              <button type="button" class="btnMoreToggle btn_morectrl">
-                <span class="ico_brunch_v1 ico_morectrl">메뉴 더보기</span>
+              <button type="button" className="btnMoreToggle btn_morectrl">
+                <span className="ico_brunch_v1 ico_morectrl">메뉴 더보기</span>
               </button>
-              <div class="layer_action_ctrl">
-                <div class="inner_action_ctrl">
-                  <button type="button" class="btnBlockProfile btn_ctrl requireLogin">
+              <div className="layer_action_ctrl">
+                <div className="inner_action_ctrl">
+                  <button type="button" className="btnBlockProfile btn_ctrl requireLogin">
                     차단하기
                   </button>
-                  <button type="button" class="btnReportProfile btn_ctrl requireLogin">
+                  <button type="button" className="btnReportProfile btn_ctrl requireLogin">
                     신고하기
                   </button>
                 </div>
@@ -76,29 +79,7 @@ function WrapHome() {
         </div>
 
         <brunchbookproject-side-banner></brunchbookproject-side-banner>
-
-        <div class="tab_contents">
-          <strong class="screen_out">작가프로필 하위메뉴</strong>
-          <ul id="contentsTab" class="list_tab">
-            <li>
-              <a href="#info" class="infoTab link_tab #info_open">
-                <span class="txt_tab">작가소개</span>
-              </a>
-            </li>
-            <li class="on">
-              <a href="#articles" class="articleTab link_tab">
-                <span class="txt_tab">글 28</span>
-              </a>
-            </li>
-            <li>
-              <a href="#works" class="magazineTab link_tab">
-                <span class="txt_tab">작품 0</span>
-              </a>
-            </li>
-          </ul>
-        </div>
       </header>
-      {/* 구분선 밑부분 부터 main 태그   article 들어가는 곳임*/}
     </>
   );
 }
