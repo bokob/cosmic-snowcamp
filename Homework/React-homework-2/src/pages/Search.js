bae2dc0ad9bd0026b4ce4fac94cc8b57cde8e7b7
bokob <bmc0520@naver.com>
2023-01-13 18:41:01 +0900

    .gitignore 수정

--- Homework/React-homework-2/src/pages/Search.js
+++ Homework/React-homework-2/src/pages/Search.js
@@ -1,5 +1,9 @@
 function Search() {
-  return <div>검색결과</div>;
+
+    // const [keyword, setKeyword] Redux 이용해서 keyword 저장한다.
+
+    const searchKeyword = document.querySelector('#keyword').value;
+  return (<div>검색결과</div>);
 }
 
 export default Search;
